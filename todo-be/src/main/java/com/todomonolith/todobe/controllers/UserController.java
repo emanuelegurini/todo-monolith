2bab31fba8a416e3f33f4478de733b1e02dda227
EmanueleGurini <emanuele.gurini89@gmail.com>
2025-10-02 13:00:11 +0200

    feat: add get user by id methods

--- todo-be/src/main/java/com/todomonolith/todobe/controllers/UserController.java
+++ todo-be/src/main/java/com/todomonolith/todobe/controllers/UserController.java
@@ -7,10 +7,12 @@ import com.todomonolith.todobe.mappers.UserMapper;
 import com.todomonolith.todobe.services.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @AllArgsConstructor
 @RestController
@@ -23,6 +25,11 @@ public class UserController {
     @GetMapping("/users")
     public List<UserDTO> getUsers() {
         return userMapper.toDTOList((List<User>) userService.findAll());
+    }
+
+    @GetMapping("/users/{id}")
+    public Optional<UserDTO> getUser(@PathVariable Long id) {
+        return userService.findById(id).map(userMapper::toDTO);
     }
 
 }
